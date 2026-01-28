ae43ea417b6f28a29b5621dd8bb38759a1a260e2
Achal Choudhary <achalchoudhary1293@gmail.com>
2026-01-28 11:44:20 +0530

    Idea Route 1.1

--- server/routes/ideaRoutes.js
+++ server/routes/ideaRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { createIdea, getIdeas, getMyIdeas } = require('../controllers/ideaController');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const authenticateToken = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
