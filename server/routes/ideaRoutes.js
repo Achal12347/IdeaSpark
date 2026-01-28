5f2fee329b7ff297117adae885599ea1751fb678
Achal Choudhary <achalchoudhary1293@gmail.com>
2026-01-28 11:40:49 +0530

    Idea Route

---
+++ server/routes/ideaRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { createIdea, getIdeas, getMyIdeas } = require('../controllers/ideaController');
+const { authenticateToken } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// POST /api/ideas - Create a new idea
+router.post('/', authenticateToken, createIdea);
+
+// GET /api/ideas - Get all ideas
+router.get('/', getIdeas);
+
+// GET /api/ideas/my - Get user's ideas
+router.get('/my', authenticateToken, getMyIdeas);
+
+module.exports = router;
